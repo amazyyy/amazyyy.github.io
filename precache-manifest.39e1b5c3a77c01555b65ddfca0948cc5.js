516305b074514517124f84f785e34ff2e8053094
Travis Helper (from Travis CI) <teamamazyyy@gmail.com>
2020-07-27 06:47:33 +0000

    Deploy amazyyy/amazyyy.github.io to github.com/amazyyy/amazyyy.github.io.git:master

---
+++ precache-manifest.39e1b5c3a77c01555b65ddfca0948cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b013c3ed9c111332745a51b39b8d02e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6fc75ba7f298180278",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "171e173592c2b3e96203",
+    "url": "/static/js/2.4e68c71c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e68c71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6fc75ba7f298180278",
+    "url": "/static/js/main.3e6b03fb.chunk.js"
+  },
+  {
+    "revision": "7e6c1524872abf197a39",
+    "url": "/static/js/runtime-main.08521ef5.js"
+  }
+]);
